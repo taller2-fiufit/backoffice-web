6915d03a16d3c513ba6431e7a18dbce4f4daca52
chiara ciriani <cciriani@kovix-group.com>
2023-04-27 19:27:54 -0300

    feat: add first metrics layout

--- src/views/Metrics.vue
+++ src/views/Metrics.vue
@@ -2,4 +2,104 @@
   <v-app-bar color="#9ACD32" class="flex-grow-0" app dark>
     <v-app-bar-title>Métricas</v-app-bar-title>
   </v-app-bar>
+
+  <v-card class="mt-3">
+    <v-tabs v-model="tab" align-tabs="center" color="primary lighten-3">
+      <v-tabs-slider></v-tabs-slider>
+      <v-tab value="users">Usuarios</v-tab>
+      <v-tab value="plans">Entrenamientos</v-tab>
+      <v-tab value="transactions">Transacciones</v-tab>
+    </v-tabs>
+
+    <v-divider class="mx-7"></v-divider>
+
+    <v-card-text>
+      <v-window v-model="tab">
+        <v-window-item value="users">
+          <v-card flat>
+            <v-container>
+              <v-row>
+                <v-col cols="6">
+                  <v-row>
+                    <v-col cols="12" class="my-0 py-0">
+                      <v-card-title>Métricas de Registro</v-card-title>
+                      <v-space></v-space>
+                      <v-card-text>Usuarios utilizando mail y contraseña</v-card-text>
+                      <v-card-text>Usuarios utilizando identidad federada</v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-col>
+                <v-divider vertical class="my-2 vertical-divider"></v-divider>
+                <v-col cols="6">
+                  <v-row>
+                    <v-col cols="12" class="my-0 py-0">
+                      <v-card-title>Métricas de Login</v-card-title>
+                      <v-space></v-space>
+                      <v-card-text>Usuarios utilizando mail y contraseña</v-card-text>
+                      <v-card-text>Usuarios utilizando identidad federada</v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-col>
+                <v-divider horizontal class="my-4 horizontal-divider"></v-divider>
+                <v-col cols="6">
+                  <v-row>
+                    <v-col cols="12" class="my-0 py-0">
+                      <v-card-title>Métricas de usuarios bloqueados</v-card-title>
+                      <v-space></v-space>
+                      <v-card-text>Usuarios bloqueados</v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-window-item>
+
+        <v-window-item value="plans">
+          <v-card flat>
+            <v-container>
+              <v-row>
+                <v-col cols="6">
+                  <v-row>
+                    <v-col cols="12" class="my-0 py-0">
+                      <v-card-title>Métricas de Nuevos Entrenamientos</v-card-title>
+                      <v-space></v-space>
+                      <v-card-text></v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-col>
+                <v-divider vertical class="my-2 vertical-divider"></v-divider>
+                <v-col cols="6">
+                  <v-row>
+                    <v-col cols="12" class="my-0 py-0">
+                      <v-card-title>Métricas de Entrenamientos</v-card-title>
+                      <v-space></v-space>
+                      <v-card-text></v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-window-item>
+
+        <v-window-item value="transactions">
+          
+        </v-window-item>
+      </v-window>
+    </v-card-text>
+  </v-card>
+
 </template>
+
+<script>
+  import MetricsService from '../services/metrics.service';
+  export default {
+    name: 'Metrics',
+    data() {
+      return {
+        tab: null,
+      };
+    },
+  }
+</script>

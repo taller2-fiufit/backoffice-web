b4e9adf2492aa3fcf6ef0f3b24b7195acc45ff3e
chiara ciriani <cciriani@kovix-group.com>
2023-04-27 19:40:47 -0300

    feat: install module for graphics

--- src/views/Metrics.vue
+++ src/views/Metrics.vue
@@ -3,7 +3,7 @@
     <v-app-bar-title>Métricas</v-app-bar-title>
   </v-app-bar>
 
-  <v-card class="mt-3">
+  <v-card class="mx-5 my-5 rounded-sm">
     <v-tabs v-model="tab" align-tabs="center" color="primary lighten-3">
       <v-tabs-slider></v-tabs-slider>
       <v-tab value="users">Usuarios</v-tab>
@@ -24,8 +24,9 @@
                     <v-col cols="12" class="my-0 py-0">
                       <v-card-title>Métricas de Registro</v-card-title>
                       <v-space></v-space>
-                      <v-card-text>Usuarios utilizando mail y contraseña</v-card-text>
-                      <v-card-text>Usuarios utilizando identidad federada</v-card-text>
+                      <v-card-text>Usuarios utilizando mail y contraseña a lo largo del tiempo</v-card-text>
+                      <v-card-text>Usuarios utilizando identidad federada a lo largo del tiempo</v-card-text>
+                      <v-card-text>Mail y Contraseña vs Identidad federada</v-card-text>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -35,8 +36,9 @@
                     <v-col cols="12" class="my-0 py-0">
                       <v-card-title>Métricas de Login</v-card-title>
                       <v-space></v-space>
-                      <v-card-text>Usuarios utilizando mail y contraseña</v-card-text>
-                      <v-card-text>Usuarios utilizando identidad federada</v-card-text>
+                      <v-card-text>Usuarios utilizando mail y contraseña a lo largo del tiempo</v-card-text>
+                      <v-card-text>Usuarios utilizando identidad federada a lo largo del tiempo</v-card-text>
+                      <v-card-text>Mail y Contraseña vs Identidad federada</v-card-text>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -46,7 +48,8 @@
                     <v-col cols="12" class="my-0 py-0">
                       <v-card-title>Métricas de usuarios bloqueados</v-card-title>
                       <v-space></v-space>
-                      <v-card-text>Usuarios bloqueados</v-card-text>
+                      <v-card-text>Usuarios bloqueados a lo largo del tiempo</v-card-text>
+                      <v-card-text>Cantidad de Usuarios Bloqueados vs No Bloqueados</v-card-text>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -84,7 +87,21 @@
         </v-window-item>
 
         <v-window-item value="transactions">
-          
+          <v-card flat>
+            <v-container>
+              <v-row>
+                <v-col cols="6">
+                  <v-row>
+                    <v-col cols="12" class="my-0 py-0">
+                      <v-card-title>Métricas de Aportes</v-card-title>
+                      <v-space></v-space>
+                      <v-card-text></v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
         </v-window-item>
       </v-window>
     </v-card-text>

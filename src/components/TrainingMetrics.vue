ac2838fb2cd519ba6439241d3514cd43f4ecd7ca
chiara ciriani <cciriani@kovix-group.com>
2023-05-09 15:31:59 -0300

    feat: finish user and training metrics structure

--- src/components/TrainingMetrics.vue
+++ src/components/TrainingMetrics.vue
@@ -1,70 +1,145 @@
 <template>
-    <v-card flat>
-        <v-container>
-          <v-card-title class="text-center pb-10">Métricas Generales</v-card-title>
+  <v-card flat>
+    <v-container>
+      <v-card-title id="title" class="text-center pb-10">Métricas Generales</v-card-title>
+      <v-row>
+        <v-col cols="6">
           <v-row>
-            <v-col cols="6">
-              <v-row>
-                <v-col cols="12" class="my-0 py-0">
-                  <v-card-title>Métricas de Nuevos Entrenamientos</v-card-title>
-                  <v-space></v-space>
-                  <v-card-text>Cantidad de Entrenamientos creados: {{this.trainingPlansMetrics.trainingsCreated}}</v-card-text>
-                </v-col>
-              </v-row>
-            </v-col>
-            <v-divider vertical class="my-2 vertical-divider"></v-divider>
-            <v-col cols="6">
-              <v-row>
-                <v-col cols="12" class="my-0 py-0">
-                  <v-card-title>Métricas de Entrenamientos por Tipo</v-card-title>
-                  <v-space></v-space>
-                  <v-card-text>Cantidad de Tipos de Entrenamientos: </v-card-text>
-                </v-col>
-              </v-row>
-            </v-col>
-            <v-divider horizontal class="my-4 horizontal-divider"></v-divider>
-            <v-card-title class="text-center py-5">Métricas Por Fecha</v-card-title>
-            <v-col cols="12">
-              <v-form fast-fail @submit.prevent="handleTrainingSelectedDates">
-                <span>Elegir Fecha de Inicio y Fin para generar los gráficos a lo largo del tiempo</span>
-                <div>
-                  <ErrorAlert v-if="trainingsError" :error="trainingsError" />
-                </div>
+            <v-col cols="12" class="my-0 py-0">
+              <v-card-title class="pb-5">Métricas de Nuevos Entrenamientos</v-card-title>
 
-                <v-text-field v-model="trainingsStartDate" name="trainingsStartDate" type="date" label="Fecha de inicio" required></v-text-field>
-                <v-text-field v-model="trainingsEndDate" name="trainingsEndDate" type="date" label="Fecha de fin" required></v-text-field>
-
-                <v-btn type="submit" color="#9ACD32" block class="mt-2">
-                  <span
-                    class="my-14 spinner-border spinner-border-sm"
-                  ></span>
-                  <span>Generar gráficos</span>
-                </v-btn> 
-              </v-form>
-            </v-col>
-            <v-space></v-space>
-            <v-col cols="6">
-              <v-row>
-                <v-col cols="12" class="my-0 py-0">
-                  <v-card-title>Métricas de Nuevos Entrenamientos</v-card-title>
-                  <v-space></v-space>
-                  <v-card-text>Cantidad de Entrenamientos creados a lo largo del tiempo</v-card-text>
-                </v-col>
-              </v-row>
-            </v-col>
-            <v-divider vertical class="my-2 vertical-divider"></v-divider>
-            <v-col cols="6">
-              <v-row>
-                <v-col cols="12" class="my-0 py-0">
-                  <v-card-title>Métricas de Entrenamientos por Tipo</v-card-title>
-                  <v-space></v-space>
-                  <v-card-text></v-card-text>
-                </v-col>
-              </v-row>
+              <v-card class="mx-auto">
+                <v-row>
+                  <v-col class="text-center mb-0 pb-5" cols="2">
+                    <v-row justify="center">
+                      <v-col cols="2">
+                        <v-sheet
+                          height="80"
+                          width="80"
+                          rounded
+                          color="#9ACD32" 
+                        >
+                          <v-container fill-height fluid>
+                            <v-row>
+                              <v-col class="text-center">
+                                <v-icon size="45">mdi-gymnastics</v-icon>
+                              </v-col>
+                            </v-row>
+                          </v-container>
+                        </v-sheet>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="10" class="mb-0 pb-0 text-center">
+                    <v-card-text v-if="this.trainingPlansMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Entrenamientos creados
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{this.trainingPlansMetrics.trainingsCreated}}
+                      </div>
+                    </v-card-text>
+                  </v-col>
+                </v-row>
+              </v-card>
             </v-col>
           </v-row>
-        </v-container>
-    </v-card>
+        </v-col>
+        <v-divider vertical class="my-2 vertical-divider"></v-divider>
+        <v-col cols="6">
+          <v-row>
+            <v-col cols="12" class="my-0 py-0">
+              <v-card-title class="pb-5">Métricas de Entrenamientos por Tipo</v-card-title>
+              
+              <v-card class="mx-auto">
+                <v-row>
+                  <v-col class="text-center mb-0 pb-5" cols="2">
+                    <v-row justify="center">
+                      <v-col cols="2">
+                        <v-sheet
+                          height="80"
+                          width="80"
+                          rounded
+                          color="#9ACD32" 
+                        >
+                          <v-container fill-height fluid>
+                            <v-row>
+                              <v-col class="text-center">
+                                <v-icon size="45">mdi-format-list-bulleted-type</v-icon>
+                              </v-col>
+                            </v-row>
+                          </v-container>
+                        </v-sheet>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="10" class="mb-0 pb-0 text-center">
+                    <v-card-text v-if="this.trainingPlansMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Cantidad de Tipos de Entrenamientos
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{}}
+                      </div>
+                    </v-card-text>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+
+
+      <v-row>
+        <v-divider id="divider" horizontal class="my-4 horizontal-divider"></v-divider>
+      </v-row>
+
+
+      <v-card-title id="title" class="text-center py-5">Métricas Por Fecha</v-card-title>
+      <v-form class="text-center" fast-fail @submit.prevent="handleTrainingSelectedDates">
+        <span>Elegir Fecha de Inicio y Fin para generar los gráficos a lo largo del tiempo</span>
+        <div>
+          <ErrorAlert v-if="trainingsError" :error="trainingsError" />
+        </div>
+
+        <v-text-field v-model="trainingsStartDate" name="trainingsStartDate" type="date" label="Fecha de inicio" required class="dateInput1"></v-text-field>
+        <v-text-field v-model="trainingsEndDate" name="trainingsEndDate" type="date" label="Fecha de fin" required class="dateInput2"></v-text-field>
+
+        <v-btn type="submit" color="#9ACD32" class="mt-2 pb-20" :disabled="loading">
+          <span
+            v-show="loading"
+            class="my-14 spinner-border spinner-border-sm"
+          ></span>
+          <span>Generar gráficos</span>
+        </v-btn> 
+
+        <v-divider horizontal class="my-4 horizontal-divider"></v-divider>
+      </v-form>
+
+      <v-row>
+        <v-col cols="6" v-if="this.trainingPlansMetricsByDate">
+          <v-row>
+            <v-col cols="12" class="my-0 py-0">
+              <v-card-title>Métricas de Nuevos Entrenamientos</v-card-title>
+              <v-space></v-space>
+              <v-card-text>Cantidad de Entrenamientos creados a lo largo del tiempo: {{this.trainingPlansMetricsByDate.trainingsCreated}}</v-card-text>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-divider vertical class="my-2 vertical-divider"></v-divider>
+        <v-col cols="6" v-if="this.trainingPlansMetricsByDate">
+          <v-row>
+            <v-col cols="12" class="my-0 py-0">
+              <v-card-title>Métricas de Entrenamientos por Tipo</v-card-title>
+              <v-space></v-space>
+              <v-card-text>Cantidad de Tipos de Entrenamientos a lo largo del tiempo: </v-card-text>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
@@ -104,8 +179,6 @@
         (response) => {
           this.trainingPlansMetrics = response.data;
           console.log(this.trainingPlansMetrics) // borrar
-
-          this.createTrainingGraphsMetrics()
         },
         (error) => {
         }
@@ -127,10 +200,11 @@
         } else {
           MetricsService.getTrainingPlansMetricsByDate(this.startDate, this.endDate).then(
             (response) => {
+              this.trainingsError = null;
               this.trainingPlansMetricsByDate = response.data;
               console.log(this.trainingPlansMetricsByDate); // borrar
 
-              this.createTrainingGraphsMetricsByDate()
+              this.createTrainingGraphsMetrics()
             },
             (error) => {
             }
@@ -140,9 +214,6 @@
       },
       // Training Graphs
       createTrainingGraphsMetrics() {
-      },
-      // Training Graphs by date
-      createTrainingGraphsMetricsByDate() {  
       }
     },
     validations() {
@@ -155,4 +226,24 @@
 </script>
 
 <style>
+#title {
+  font-size: 25px;
+}
+
+.dateInput1 {
+  width: 20%;
+  margin: auto;
+  padding-top: 20px;
+}
+
+.dateInput2 {
+  width: 20%;
+  margin: auto;
+}
+
+#divider {
+  border-width: 20px !important;
+  border-color: #9ACD32 !important;
+  width: 100%;
+}
 </style>
--- src/components/UserMetrics.vue
+++ src/components/UserMetrics.vue
@@ -1,15 +1,55 @@
 <template>
-<v-card flat>
+  <v-card flat>
     <v-container>
-      <v-card-title class="text-center pb-10">Métricas Generales</v-card-title>
+      <v-card-title id="title" class="text-center pb-10">Métricas Generales</v-card-title>
       <v-row>
         <v-col cols="6">
           <v-row>
             <v-col cols="12" class="my-0 py-0">
-              <v-card-title>Métricas de Registro</v-card-title>
-              <v-space></v-space>
-              <v-card-text v-if="this.usersMetrics">Usuarios registrados utilizando mail y contraseña: {{this.usersMetrics.signinsWithMail}}</v-card-text>
-              <v-card-text v-if="this.usersMetrics">Usuarios registrados utilizando identidad federada: {{this.usersMetrics.signinsWithFederatedId}}</v-card-text>
+              <v-card-title class="pb-5">Métricas de Registro</v-card-title>
+
+              <v-card class="mx-auto">
+                <v-row>
+                  <v-col class="text-center mb-0 pb-0" cols="2">
+                    <v-row justify="center">
+                      <v-col cols="2">
+                        <v-sheet
+                          height="80"
+                          width="80"
+                          rounded
+                          color="#9ACD32" 
+                        >
+                          <v-container fill-height fluid>
+                            <v-row>
+                              <v-col class="text-center">
+                                <v-icon size="45">mdi-account-plus</v-icon>
+                              </v-col>
+                            </v-row>
+                          </v-container>
+                        </v-sheet>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="10" class="mb-0 pb-0 text-center">
+                    <v-card-text v-if="this.usersMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Utilizando mail y contraseña
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{this.usersMetrics.signinsWithMail}}
+                      </div>
+                    </v-card-text>
+                    <v-card-text v-if="this.usersMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Utilizando identidad federada
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{this.usersMetrics.signinsWithFederatedId}}
+                      </div>
+                    </v-card-text>
+                  </v-col>
+                </v-row>
+              </v-card>
             </v-col>
           </v-row>
         </v-col>
@@ -17,10 +57,50 @@
         <v-col cols="6">
           <v-row>
             <v-col cols="12" class="my-0 py-0">
-              <v-card-title>Métricas de Login</v-card-title>
-              <v-space></v-space>
-              <v-card-text v-if="this.usersMetrics">Usuarios loggueados utilizando mail y contraseña: {{this.usersMetrics.loginsWithMail}}</v-card-text>
-              <v-card-text v-if="this.usersMetrics">Usuarios loggueados utilizando identidad federada:{{this.usersMetrics.loginsWithFederatedId}}</v-card-text>
+              <v-card-title class="pb-5">Métricas de Login</v-card-title>
+
+              <v-card class="mx-auto">
+                <v-row>
+                  <v-col class="text-center mb-0 pb-0" cols="2">
+                    <v-row justify="center">
+                      <v-col cols="2">
+                        <v-sheet
+                          height="80"
+                          width="80"
+                          rounded
+                          color="#9ACD32" 
+                        >
+                          <v-container fill-height fluid>
+                            <v-row>
+                              <v-col class="text-center">
+                                <v-icon size="45">mdi-login</v-icon>
+                              </v-col>
+                            </v-row>
+                          </v-container>
+                        </v-sheet>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="10" class="mb-0 pb-0 text-center">
+                    <v-card-text v-if="this.usersMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Utilizando mail y contraseña
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{this.usersMetrics.loginsWithMail}}
+                      </div>
+                    </v-card-text>
+                    <v-card-text v-if="this.usersMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Utilizando identidad federada
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{this.usersMetrics.loginsWithFederatedId}}
+                      </div>
+                    </v-card-text>
+                  </v-col>
+                </v-row>
+              </v-card>
             </v-col>
           </v-row>
         </v-col>
@@ -28,39 +108,90 @@
         <v-col cols="6">
           <v-row>
             <v-col cols="12" class="my-0 py-0">
-              <v-card-title>Métricas de Usuarios Bloqueados</v-card-title>
-              <v-space></v-space>
-              <v-card-text v-if="this.usersMetrics">Cantidad de Usuarios: {{this.usersMetrics.signinsWithMail + this.usersMetrics.signinsWithFederatedId}}</v-card-text>
-              <v-card-text v-if="this.usersMetrics">Cantidad de Usuarios Bloqueados: {{this.usersMetrics.blockedUsers}}</v-card-text>
+              <v-card-title class="pb-5">Métricas de Usuarios Bloqueados</v-card-title>
+              
+              <v-card class="mx-auto">
+                <v-row>
+                  <v-col class="text-center mb-0 pb-0" cols="2">
+                    <v-row justify="center">
+                      <v-col cols="2">
+                        <v-sheet
+                          height="80"
+                          width="80"
+                          rounded
+                          color="#9ACD32" 
+                        >
+                          <v-container fill-height fluid>
+                            <v-row>
+                              <v-col class="text-center">
+                                <v-icon size="45">mdi-block-helper</v-icon>
+                              </v-col>
+                            </v-row>
+                          </v-container>
+                        </v-sheet>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  <v-col cols="10" class="mb-0 pb-0 text-center">
+                    <v-card-text v-if="this.usersMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Cantidad de Usuarios
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{this.usersMetrics.signinsWithMail + this.usersMetrics.signinsWithFederatedId}}
+                      </div>
+                    </v-card-text>
+                    <v-card-text v-if="this.usersMetrics" class="pt-0">
+                      <div class="overline grey--text" style="font-size: 15px !important;">
+                        Cantidad de Usuarios Bloqueados
+                      </div>
+                      <div class="headline font-weight-medium grey--text text-center">
+                        {{this.usersMetrics.blockedUsers}}
+                      </div>
+                    </v-card-text>
+                  </v-col>
+                </v-row>
+              </v-card>
             </v-col>
           </v-row>
         </v-col>
+      </v-row>
+
+
+      <v-row>
+        <v-divider id="divider" horizontal class="my-4 horizontal-divider"></v-divider>
+      </v-row>
+
+
+      <v-card-title id="title" class="text-center py-5">Métricas Por Fecha</v-card-title>
+      <v-form class="text-center" fast-fail @submit.prevent="handleUserSelectedDates">
+        <span>Elegir Fecha de Inicio y Fin para generar los gráficos a lo largo del tiempo</span>
+        <div>
+          <ErrorAlert v-if="usersError" :error="usersError" />
+        </div>
+
+        <v-text-field v-model="usersStartDate" name="usersStartDate" type="date" label="Fecha de inicio" required class="dateInput1"></v-text-field>
+        <v-text-field v-model="usersEndDate" name="usersEndDate" type="date" label="Fecha de fin" required class="dateInput2"></v-text-field>
+        
+        <v-btn type="submit" color="#9ACD32" class="mt-2 pb-20" :disabled="loading">
+          <span
+            v-show="loading"
+            class="my-14 spinner-border spinner-border-sm"
+          ></span>
+          <span>Generar gráficos</span>
+        </v-btn> 
+
         <v-divider horizontal class="my-4 horizontal-divider"></v-divider>
-        <v-card-title class="text-center py-5">Métricas Por Fecha</v-card-title>
-        <v-col cols="12">
-          <v-form fast-fail @submit.prevent="handleUserSelectedDates">
-            <span>Elegir Fecha de Inicio y Fin para generar los gráficos a lo largo del tiempo</span>
-            <div>
-              <ErrorAlert v-if="usersError" :error="usersError" />
-            </div>
-            <v-text-field v-model="usersStartDate" name="usersStartDate" type="date" label="Fecha de inicio" required></v-text-field>
-            <v-text-field v-model="usersEndDate" name="usersEndDate" type="date" label="Fecha de fin" required></v-text-field>
-            <v-btn type="submit" color="#9ACD32" block class="mt-2">
-              <span
-                class="my-14 spinner-border spinner-border-sm"
-              ></span>
-              <span>Generar gráficos</span>
-            </v-btn> 
-          </v-form>
-        </v-col>
-        <v-space></v-space>
-        <v-col cols="6">
+      </v-form>
+
+      <v-row>
+        <v-col cols="6" v-if="this.usersMetricsByDate">
           <v-row>
             <v-col cols="12" class="my-0 py-0">
               <v-card-title>Métricas de Registro</v-card-title>
               <v-space></v-space>
-              <v-card-text>Usuarios utilizando mail y contraseña a lo largo del tiempo</v-card-text>
-              <v-card-text>Usuarios utilizando identidad federada a lo largo del tiempo</v-card-text>
+              <v-card-text>Usuarios utilizando mail y contraseña a lo largo del tiempo: {{this.usersMetricsByDate.signinsWithMail}}</v-card-text>
+              <v-card-text>Usuarios utilizando identidad federada a lo largo del tiempo: {{this.usersMetricsByDate.signinsWithFederatedId}}</v-card-text>
               <v-card-text>Mail y Contraseña vs Identidad federada</v-card-text>
               <div>
                 <BarChart class="bar-chart" v-bind="this.userSigninsMailVsFederatedIdBarChartProps" />
@@ -69,13 +200,13 @@
           </v-row>
         </v-col>
         <v-divider vertical class="my-2 vertical-divider"></v-divider>
-        <v-col cols="6">
+        <v-col cols="6" v-if="this.usersMetricsByDate">
           <v-row>
             <v-col cols="12" class="my-0 py-0">
               <v-card-title>Métricas de Login</v-card-title>
               <v-space></v-space>
-              <v-card-text>Usuarios utilizando mail y contraseña a lo largo del tiempo</v-card-text>
-              <v-card-text>Usuarios utilizando identidad federada a lo largo del tiempo</v-card-text>
+              <v-card-text>Usuarios utilizando mail y contraseña a lo largo del tiempo: {{this.usersMetricsByDate.loginsWithMail}}</v-card-text>
+              <v-card-text>Usuarios utilizando identidad federada a lo largo del tiempo: {{this.usersMetricsByDate.loginsWithFederatedId}}</v-card-text>
               <v-card-text>Mail y Contraseña vs Identidad federada</v-card-text>
               <div>
                 <BarChart class="bar-chart" v-bind="this.userLoginsMailVsFederatedIdBarChartProps" />
@@ -84,13 +215,14 @@
           </v-row>
         </v-col>
         <v-divider horizontal class="my-4 horizontal-divider"></v-divider>
-        <v-col cols="6">
+        <v-col cols="6" v-if="this.usersMetricsByDate">
           <v-row>
             <v-col cols="12" class="my-0 py-0">
               <v-card-title>Métricas de Usuarios Bloqueados</v-card-title>
               <v-space></v-space>
-              <v-card-text>Usuarios bloqueados a lo largo del tiempo</v-card-text>
-              <v-card-text>Cantidad de Usuarios Bloqueados vs No Bloqueados</v-card-text>
+              <v-card-text>Usuarios a lo largo del tiempo: {{this.usersMetricsByDate.signinsWithMail + this.usersMetricsByDate.signinsWithFederatedId}}</v-card-text>
+              <v-card-text>Usuarios bloqueados a lo largo del tiempo: {{this.usersMetricsByDate.blockedUsers}}</v-card-text>
+              <v-card-text>Cantidad de Usuarios Bloqueados vs No Bloqueados: </v-card-text>
               <div>
                 <BarChart class="bar-chart" v-bind="this.userBlockedVsNotBlockedBarChartProps" />
               </div>
@@ -142,8 +274,6 @@
         (response) => {
           this.usersMetrics = response.data;
           console.log(this.usersMetrics); // borrar
-
-          this.createUserGraphsMetrics()
         },
         (error) => {
         }
@@ -165,10 +295,11 @@
         } else {
           MetricsService.getUsersMetricsByDate(this.startDate, this.endDate).then(
             (response) => {
+              this.usersError = null;
               this.usersMetricsByDate = response.data;
               console.log(this.usersMetricsByDate); // borrar
 
-              this.createUserGraphsMetricsByDate()
+              this.createUserGraphsMetrics()
             },
             (error) => {
             }
@@ -185,7 +316,7 @@
       },
       // Métricas de Registro: Mail y Contraseña vs Identidad federada
       createUserSigninsMailVsFederatedIdChart() {
-        const userRegisterMetrics = Object.values(this.usersMetrics);
+        const userRegisterMetrics = Object.values(this.usersMetricsByDate);
         //                signinsWithMail,        signinsWithFederatedId
         const data = ref([userRegisterMetrics[0], userRegisterMetrics[1]]);
 
@@ -205,7 +336,7 @@
       },
       // Métricas de Login: Mail y Contraseña vs Identidad federada
       createUserLoginsMailVsFederatedIdChart() { 
-        const userLoginMetrics = Object.values(this.usersMetrics);
+        const userLoginMetrics = Object.values(this.usersMetricsByDate);
         //                loginsWithMail, loginsWithFederatedId
         const data = ref([userLoginMetrics[2], userLoginMetrics[3]]);
 
@@ -224,7 +355,7 @@
         this.userLoginsMailVsFederatedIdBarChartProps = barChartProps;
       },
       createUserBlockedVsNotBlockedBarChart() {
-        const userBlockedMetrics = Object.values(this.usersMetrics);
+        const userBlockedMetrics = Object.values(this.usersMetricsByDate);
         //                 signinsWithMail      +  signinsWithFederatedId,                       blockedUsers
         const data = ref([userBlockedMetrics[0] + userBlockedMetrics[1] - userBlockedMetrics[4], userBlockedMetrics[4]]);
 
@@ -242,10 +373,6 @@
 
         this.userBlockedVsNotBlockedBarChartProps = barChartProps;
       },
-      // User Graphs by date
-      createUserGraphsMetricsByDate() {
-
-      }
     },
     validations() {
       return {
@@ -257,9 +384,31 @@
 </script>
 
 <style>
+#title {
+  font-size: 25px;
+}
+
 .bar-chart {
   height: 300px;
   width: 600px;
   align-self: center;
 }
+
+.dateInput1 {
+  width: 20%;
+  margin: auto;
+  padding-top: 20px;
+}
+
+.dateInput2 {
+  width: 20%;
+  margin: auto;
+}
+
+#divider {
+  border-width: 20px !important;
+  border-color: #9ACD32 !important;
+  width: 100%;
+}
+
 </style>
